af2ba7dbc09b05a33d34f15506b710805d5eda8a
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-07 15:55:08 +0900

    Update Header.tsx

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 const HeaderBlock = styled.header`
   width: 100%;
-  padding: 1rem 2rem;
-  justify-content: space-around;
+  padding: 1rem 5rem;
+  justify-content: space-between;
 
   background-color: #222;
   color: #eee;
@@ -14,18 +14,18 @@ const HeaderBlock = styled.header`
 const Header = () => {
   return (
     <HeaderBlock className="flex ai-center">
-      <div className="flex jc-center ai-center">
+      <div className="flex jc-center">
         <Link href="/">
           <a>
-            <Image src="/NETFLEX.svg" width={100} height={10} />
+            <Image src="/NETFLEX.svg" width={200} height={20} />
           </a>
         </Link>
-        <Link href="/Movie">
+        {/* <Link href="/Movie">
           <a style={{ marginRight: '1rem' }}>Movie</a>
         </Link>
         <Link href="/TV">
           <a style={{ marginRight: '1rem' }}>TV</a>
-        </Link>
+        </Link> */}
       </div>
       <div className="flex jc-center ai-center">
         {
