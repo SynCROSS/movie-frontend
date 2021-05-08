3bd5b056d8da403e19201fdb46a17db2e9d3189d
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-08 16:14:16 +0900

    Update Header.tsx

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -6,9 +6,21 @@ const HeaderBlock = styled.header`
   width: 100%;
   padding: 1rem 5rem;
   justify-content: space-between;
+  position: sticky;
+  top: 0;
+  left: 0;
+  /* z-index: 10; */
 
-  background-color: #222;
+  background-color: #000;
   color: #eee;
+`;
+
+const MenuLink = styled.a`
+  margin: 0 1rem;
+
+  &:hover {
+    color: #ddd;
+  }
 `;
 
 const Header = () => {
@@ -17,15 +29,15 @@ const Header = () => {
       <div className="flex jc-center">
         <Link href="/">
           <a>
-            <Image src="/NETFLEX.svg" width={200} height={20} />
+            <Image src="/NETFLEX.svg" width={140} height={20} />
           </a>
         </Link>
-        {/* <Link href="/Movie">
-          <a style={{ marginRight: '1rem' }}>Movie</a>
+        <Link href="/Movie">
+          <MenuLink>Movie</MenuLink>
         </Link>
         <Link href="/TV">
-          <a style={{ marginRight: '1rem' }}>TV</a>
-        </Link> */}
+          <MenuLink>TV</MenuLink>
+        </Link>
       </div>
       <div className="flex jc-center ai-center">
         {
