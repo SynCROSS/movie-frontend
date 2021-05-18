d7735ad35a9cf23502122904a5512eab2815e245
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-18 10:52:19 +0900

    Update link.ts

--- lib/link.ts
+++ lib/link.ts
@@ -1,2 +1,2 @@
-export const PLACEHOlDER_URL = 'https://via.placeholder.com/326x183/000?Text=';
+export const PLACEHOlDER_URL = 'https://via.placeholder.com/326x183/000?text=';
 export const BASE_IMAGE_URL = 'https://www.themoviedb.org/t/p/original';
