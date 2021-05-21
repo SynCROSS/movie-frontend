2345cbb9151084c9db9ac71320c6eae10761367f
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-21 16:54:39 +0900

    Update auth.ts

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -6,6 +6,7 @@ export const login = async ({ username, password }) => {
       username,
       password,
     });
+
     return data?.access_token;
   } catch (e) {
     console.error(e);
@@ -14,12 +15,13 @@ export const login = async ({ username, password }) => {
 
 export const register = async ({ username, nickname, email, password }) => {
   try {
-    return await client.post(`${BASE_URL}/users/register`, {
+    const { data } = await client.post(`${BASE_URL}/users/register`, {
       username,
       nickname,
       email,
       password,
     });
+    return data;
   } catch (e) {
     console.error(e);
   }
@@ -27,11 +29,12 @@ export const register = async ({ username, nickname, email, password }) => {
 
 export const checkLoggedIn = async (token: string) => {
   try {
-    return await client.get(`${BASE_URL}/auth/profile`, {
+    const { data } = await client.get(`${BASE_URL}/auth/profile`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
+    return data;
   } catch (e) {
     console.error(e);
   }
